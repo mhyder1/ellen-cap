a177f937d59f19ebef2e685d41cec21e8c1ca259
Ellen Longley <ellenharperlongley@gmail.com>
2022-12-05 15:08:25 -0500

    us1 test fixes

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -58,30 +58,34 @@ function validDate(req, res, next) {
   return next();
 }
 
-// function validTime(req, res, next) {
-//   const time = req.body.data.reservation_time;
-//   const timeFormat = /\d\d:\d\d/;
-//   if (timeFormat.test(time)) {
-//     next();
-//   }
-//   return next({
-//     status: 400,
-//     message: `Invalid field(s): reservation_time`,
-//   });
-//   next();
-// }
+function validTime(req, res, next) {
+  const isValid = /^([0-1][0-9]|2[0-3]):([0-5][0-9])$/.test(req.body.data.reservation_time);
+  // const timeFormat = /\d\d:\d\d/;
+  if (isValid) {
+    return next();
+  }
+  return next({
+    status: 400,
+    message: `Invalid field(s): reservation_time`,
+  });
+}
 
 
 function hasPeople(req, res, next) {
-  if (isNaN(req.body.data.people)) {
-    next({status: 400, message: `Invalid field(s): people`})
-  }
   const people = Number(req.body.data.people)
   if (people >= 1) {
      return next()
   } else {
-    next({status: 400, message: `Invalid field(s): people`})
+    return next({status: 400, message: `Invalid field(s): people`})
   }
+}
+
+function peopleNumber(req, res, next) {
+  // checks the type of the data coming through, because we want a string version of a number to fail (ie, "2" should fail)
+  if (typeof req.body.data.people !== "number") {
+    return next({status: 400, message: `Invalid field(s): people amount must be a number.`})
+  }
+  return next();
 }
 
 
@@ -100,5 +104,5 @@ async function list(req, res) {
 
 module.exports = {
   list: asyncErrorBoundary(list),
-  create: [hasOnlyValidProperties, hasRequiredProperties, hasPeople, validDate, asyncErrorBoundary(create)],
+  create: [hasOnlyValidProperties, hasRequiredProperties, hasPeople, peopleNumber, validDate, validTime, asyncErrorBoundary(create)],
 };
